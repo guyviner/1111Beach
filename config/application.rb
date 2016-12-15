99f7014fbf7ebf2468eb5a78e1cc6c157d5d80ed
Guy Viner <gregorious@gmail.com>
2016-12-15 13:41:21 -0800

    gitignore and time zone

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ Bundler.require(*Rails.groups)
 
 module FinalProject2
   class Application < Rails::Application
-    
+
+    config.time_zone = 'Pacific Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
